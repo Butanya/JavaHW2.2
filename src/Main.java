3c2570d8bda41fbecdf5ad154c5f857863ba4412
Tanya <tosi4bosi@gmail.com>
2022-05-05 00:13:54 +0300

    fix condition

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int initialAmount = 100;
-        int payment = 1_600;
+        int initialAmount = 0;
+        int payment = 1_100;
         int totalAmount = initialAmount + payment;
         int bonus = payment / 100;
 
         int outAmount;
-        if (payment >= 1_000) {
+        if (payment > 1_000) {
             outAmount = (totalAmount + bonus);
 
             System.out.println("На вашем счету " + outAmount + " рублей, из них бонусных рублей - " + bonus);
